2a2fe6550fff86c9e3d346be795c7be91355d974
Estevam Rodrigues <estevam.mrodrigues@gmail.com>
2017-03-16 19:42:11 +0000

    Rest of CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-
   def index 
     @tasks = Task.all
   end
@@ -9,13 +8,38 @@ class TasksController < ApplicationController
   end
 
   def new 
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def create
     @task = Task.new(task_params)
 
-    @task.save
-    redirect_to @task
+    if @task.save
+      redirect_to @task
+    else 
+      render 'new'
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
   end
 
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :subject, presence: true,
+                      length: { minimum: 5 }
 end
